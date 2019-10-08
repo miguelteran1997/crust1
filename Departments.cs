90a81c210b22a5dd96f35813bd1bcaa75313fe6b
miguelteran1997 <54339141+miguelteran1997@users.noreply.github.com>
2019-10-08 12:56:01 -0500

    Add files via upload

---
+++ Departments.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD1.Data
+{
+    public class Department
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DID { get; set; }
+        public String DName { get; set; }
+    }
+}
---
+++ Employees.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD1.Data
+{
+    public class Employees
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int EID { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        [ForeignKey("DID")]
+        public Departments DeptID { get; set; }
+
+    }
+}
